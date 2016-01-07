66a8350b9776ea2255469d4c76eedf9bfc4407a9
Alex Zhang <alex8224@gmail.com>
2016-01-07 19:15:43 +0800

    rename gtornado to gTornado

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 # -*- coding:utf-8 -*-
 from setuptools import setup, find_packages
 setup(
-    name = "gtornado",
+    name = "gTornado",
     version = "0.1",
     packages = find_packages(),
     install_requires = ['tornado>=4.3', 'greenlet', "pymemcache>=1.3.5", "PyMySQL>=0.6.7"],
     author = "alexzhang",
     author_email = "alex8224@gmail.com",
-    description = "gtornado add greenify support to tornado. inspired by motor and gevent",
+    description = "gTornado add greenify support to tornado. inspired by motor and gevent",
     license = "MIT",
     keywords = "tornado greenify async",
-    url = "https://github.com/alex8224/gtornado",
+    url = "https://github.com/alex8224/gTornado",
 )
 
