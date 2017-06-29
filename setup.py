f88ed440ba9bfd3b28d93ec6b42dbba4569afeac
zhu327 <zhu327@gmail.com>
2017-06-29 19:55:02 +0800

    fix setup requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="greentor",
     version="0.1",
     packages=find_packages(),
-    install_requires=['tornado>=4.3', 'greenlet', "PyMySQL>=0.7.4"],
+    install_requires=['tornado==4.3', 'greenlet', "PyMySQL==0.7.4"],
     author="Timmy",
     author_email="zhu327@gmail.com",
     description="Greenlet support to tornado. inspired by motor and gevent",
